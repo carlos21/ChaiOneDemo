fedadefef247aa81cf2a1657f2473efec83e7ffd
Carlos Duclos <darkzeratul64@gmail.com>
2022-01-21 11:50:05 -0500

    Initial Commit

---
+++ ChaiOneDemo/ChaiOneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaiOneDemoApp.swift
+//  ChaiOneDemo
+//
+//  Created by Carlos Duclos on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ChaiOneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
